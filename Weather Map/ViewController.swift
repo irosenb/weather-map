1a710702e2996dad0ce5e8528133ef20657105f8
Isaac Rosenberg <irosenb7@gmail.com>
2014-08-09 17:07:29 -0700

    Initial Commit

---
+++ Weather Map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather Map
+//
+//  Created by Isaac Rosenberg on 8/9/14.
+//  Copyright (c) 2014 Isaac Rosenberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
