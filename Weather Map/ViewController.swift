908b93693bb1b8a7d2c51aec5ab6f26e0aae406c
Isaac Rosenberg <irosenb7@gmail.com>
2014-08-26 15:21:18 -0700

    lez push

--- Weather Map/ViewController.swift
+++ Weather Map/ViewController.swift
@@ -7,11 +7,67 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    
+    override func loadView() {
+        super.loadView()
+        
+        let contentview = UIView()
+        contentview.backgroundColor = UIColor.whiteColor()
+        self.view = contentview
+        
+        let centerview = MKMapView()
+        centerview.setTranslatesAutoresizingMaskIntoConstraints(false)
+        self.view.addSubview(centerview)
+        
+        let label = UILabel()
+        label.setTranslatesAutoresizingMaskIntoConstraints(false)
+        label.text = "hello world"
+        
+        self.view.addConstraint(NSLayoutConstraint(
+            item: centerview,
+            attribute: NSLayoutAttribute.Width,
+            relatedBy: NSLayoutRelation.Equal,
+            toItem: self.view,
+            attribute: NSLayoutAttribute.Width,
+            multiplier: 1,
+            constant: 0))
+        
+        self.view.addConstraint(NSLayoutConstraint(
+            item: centerview,
+            attribute: NSLayoutAttribute.Height,
+            relatedBy: NSLayoutRelation.Equal,
+            toItem: self.view,
+            attribute: NSLayoutAttribute.Height,
+            multiplier: 0.5,
+            constant: 0))
+        
+        self.view.addConstraint(NSLayoutConstraint(
+            item: centerview,
+            attribute: NSLayoutAttribute.CenterX,
+            relatedBy: NSLayoutRelation.Equal,
+            toItem: self.view,
+            attribute: NSLayoutAttribute.CenterX,
+            multiplier: 1,
+            constant: 0))
+        
+        self.view.addConstraint(NSLayoutConstraint(
+            item: centerview,
+            attribute: NSLayoutAttribute.TopMargin,
+            relatedBy: NSLayoutRelation.Equal,
+            toItem: self.view,
+            attribute: NSLayoutAttribute.TopMargin,
+            multiplier: 1,
+            constant: 0))
+
+        
+    }
                             
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
